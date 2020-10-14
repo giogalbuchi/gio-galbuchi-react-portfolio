67943212065fe8b7da2aaa9938bbd7d14e65ef8e
giogalbuchi <ggalbuchi@gmail.com>
2020-10-14 14:13:11 -0300

    first update

---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+import PortfolioItem from './portfolio-item';
+
+export default class PorfolioContainer extends Component {
+    constructor() {
+        super();
+        
+        this.state = {
+            pageTitle: "Welcome to my portfolio",
+            data: [
+                { title: "Quip" },
+                { title: "Eventbrite" },
+                { title: "Ministry Safe" },
+                { title: "SwingAway" }
+            ]
+        };
+    }
+
+    portfolioItems() {
+      return this.state.data.map(item => {
+          return <PortfolioItem title={item.title} />;
+      })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h2>{this.state.pageTitle}</h2>
+
+
+                {this.portfolioItems()}
+            </div>
+        )
+    }
+}
