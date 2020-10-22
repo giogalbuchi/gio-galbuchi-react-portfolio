be82486d1c7f0e8ff24948d3750e17e50712ce0c
giogalbuchi <ggalbuchi@gmail.com>
2020-10-22 13:04:21 -0300

    Developed Portfolio Manager Component

--- src/components/app.js
+++ src/components/app.js
@@ -11,6 +11,7 @@ import Home from './pages/home';
 import About from './pages/about';
 import Contact from './pages/contact';
 import Blog from './pages/blog';
+import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from './portfolio/portfolio-detail.js';
 import Auth from './pages/auth';
 import NoMatch from './pages/no-match';
@@ -78,7 +79,7 @@ export default class App extends Component {
   }
 
   authorizePages() {
-    return [<Route path='/blog' component ={Blog} />];
+    return [<Route  key="portfolio-manager" path="/portfolio-manager" component={PortfolioManager} />];
   }
 
   render() {
@@ -91,7 +92,6 @@ export default class App extends Component {
           handleSuccessfulLogout={this.handleSuccessfulLogout} 
           />
 
-          <h2>{this.state.loggedInStatus}</h2>
 
           <Switch>
             <Route exact path="/" component ={Home} />
@@ -109,6 +109,7 @@ export default class App extends Component {
 
             <Route path='/about-me' component ={About} />
             <Route path='/contact' component ={Contact} />
+            <Route path='/blog' component ={Blog} />
             {this.state.loggedInStatus === "LOGGED_IN" ? this.authorizePages() : null}
             <Route exact path='/portfolio/:slug' component={PortfolioDetail} />
             <Route component={NoMatch} />
--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -7,8 +7,8 @@ const NavigationComponent = (props) => {
     const dynamicLink = (route, linkText) => {
         return (
             <div className="nav-link-wrapper">
-                <NavLink to="/blog" activeClassName="nav-link-active">
-                    Blog
+                <NavLink to={route} activeClassName="nav-link-active">
+                    {linkText}
                 </NavLink>
             </div>
         );
@@ -46,9 +46,15 @@ const NavigationComponent = (props) => {
                             Contact
                         </NavLink>
                     </div>
+
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/blog" activeClassName="nav-link-active">
+                            Blog
+                        </NavLink>
+                    </div>
                     
 
-                    {props.loggedInStatus === "LOGGED_IN" ? dynamicLink("/blog", "Blog") : null}
+                    {props.loggedInStatus === "LOGGED_IN" ? dynamicLink("/portfolio-manager", "Portfolio Manager") : null}
                 </div>
 
                 <div className="right-side">
